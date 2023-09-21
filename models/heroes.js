a8b55e3155eb11c97dcdb1de54be10c37f048e92
Genokrad <zeeper90@gmail.com>
2023-09-21 12:11:12 +0300

    hero by id, http error middlewar

--- models/heroes.js
+++ models/heroes.js
@@ -1,10 +1,18 @@
-const heroes = require("./heroes.json");
+const fs = require("fs").promises;
+const path = require("path");
+
+const heroesPath = path.join(__dirname, "heroes.json");
 
 const listHeroes = async () => {
-  return heroes;
+  const data = await fs.readFile(heroesPath, "utf8");
+  return JSON.parse(data);
 };
 
-const getHeroById = async (heroId) => {};
+const getHeroById = async (heroId) => {
+  const heroes = await listHeroes();
+  const result = heroes.find((item) => item.id === heroId);
+  return result || null;
+};
 
 const removeHero = async (heroId) => {};
 
--- routes/api/heroes.js
+++ routes/api/heroes.js
@@ -4,13 +4,40 @@ const router = express.Router();
 
 const heroes = require("../../models/heroes");
 
+const { HttpError } = require("../../helpers");
+
 router.get("/", async (req, res, next) => {
-  const result = await heroes.listHeroes();
-  res.json(result);
+  try {
+    const result = await heroes.listHeroes();
+    res.json(result);
+  } catch (error) {
+    next(error);
+    // res.status(500).json({
+    //   message: "Server error",
+    // });
+  }
 });
 
 router.get("/:heroId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const { heroId } = req.params;
+    const result = await heroes.getHeroById(heroId);
+    if (result) {
+      res.json(result);
+    } else {
+      throw HttpError(404, "Hero not found");
+      // const error = new Error("Hero not found");
+      // error.status = 404;
+      // throw error;
+      // res.status(404).json({ message: "Hero not found" });
+    }
+  } catch (error) {
+    next(error);
+    // const { status = 500, message = "Server error" } = error;
+    // res.status(status).json({
+    //   message,
+    // });
+  }
 });
 
 router.post("/", async (req, res, next) => {
